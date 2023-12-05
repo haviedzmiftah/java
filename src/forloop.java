a3bb9db68263ab13a3a7ab852dd13a9da7c63191
haviedzmiftah <haviedzmiftah@gmail.com>
2023-12-05 14:39:03 +0700

    for loop dan if else

--- src/forloop.java
+++ src/forloop.java
@@ -1,5 +1,9 @@
 public class forloop {
     public static void main(String[] args) {
-        for (){
+        for (int i=1; i<=10; i++){ //init, kondisi, post
+            System.out.println("ini perulangan ke "+i); //blok kode perulangan
+        }
+        System.out.println("looping berakhir");
+
     }
 }
--- src/ifelse.java
+++ src/ifelse.java
@@ -1,2 +1,19 @@
 public class ifelse {
+    public static void main(String[] args) {
+        int a=7;
+        int b=2;
+
+        if (a<b){ //kondisi
+            System.out.println(a+" lebih kecil dari "+b); //jika kondisi true akan dieksekusi
+        }
+        else {
+            System.out.println(a+" tidak lebih kecil dari pada "+b); //jika kondisi false
+        }
+
+        //short if else
+        // variable = (kondisi) ? benar : salah;
+        int nilai = 74;
+        String ucapan = (nilai >= 75) ? "Selamat anda lulus":"anda tidak lulus";
+        System.out.println(ucapan);
+    }
 }
