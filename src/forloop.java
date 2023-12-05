2d5ec5510c654b52132f772850c093cd216f7dc6
haviedzmiftah <haviedzmiftah@gmail.com>
2023-12-05 16:48:47 +0700

    for loop

--- src/forloop.java
+++ src/forloop.java
@@ -3,7 +3,5 @@ public class forloop {
         for (int i=1; i<=10; i++){ //init, kondisi, post
             System.out.println("ini perulangan ke "+i); //blok kode perulangan
         }
-        System.out.println("looping berakhir");
-
     }
 }
