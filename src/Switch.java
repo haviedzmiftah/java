779eecdbf1666517022f90c45e3dccb516ec90d0
haviedzmiftah <haviedzmiftah@gmail.com>
2023-12-05 14:37:03 +0700

    switch

--- src/Switch.java
+++ src/Switch.java
@@ -1,2 +1,17 @@
 public class Switch {
+    public static void main(String[] args) {
+        var nilai = "A";
+        switch (nilai){
+            case "A":
+                System.out.println("nilai anda baik terus pertahankan");
+                break;
+            case "B" :
+                System.out.println("nilai anda baik terus tingkatkan");
+                break;
+            case "C":
+                System.out.println("Nilai anda cukup terus semangat");
+            default:
+                System.out.println("nilai anda belum cukup");
+        }
+    }
 }
